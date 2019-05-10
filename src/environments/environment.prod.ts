6b5dab0f75d515077027295160ff2b6ed90757ad
ChusLuces <chuslucesleon@hotmail.com>
2019-05-10 10:54:52 +0200

    'pub'

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: true
+  appName: 'Entorno de producción',
+  production: true,
+  projects: [
+    { id: 0, name: 'Learn Angular' },
+    { id: 1, name: 'Develop My Dream app' },
+    { id: 2, name: 'Travel around the world' },
+    { id: 3, name: 'Clean my room' }
+  ]
 };
